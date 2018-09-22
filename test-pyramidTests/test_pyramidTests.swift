53c12d66e28d3e290a66a58fa0f140d399dfb2fe
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-09-22 10:18:42 +0200

    Initial Commit

---
+++ test-pyramidTests/test_pyramidTests.swift
@@ -0,0 +1,36 @@
+//
+//  test_pyramidTests.swift
+//  test-pyramidTests
+//
+//  Created by Bartłomiej Nowak on 22.09.2018.
+//  Copyright © 2018 Bartłomiej Nowak. All rights reserved.
+//
+
+import XCTest
+@testable import test_pyramid
+
+class test_pyramidTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
